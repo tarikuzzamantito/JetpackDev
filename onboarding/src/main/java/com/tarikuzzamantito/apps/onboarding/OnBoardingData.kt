7af18c963cf2010e232f47219487632943fde2ec
Md.Tarikuzzaman <tarikuzzaman1@gmail.com>
2024-08-12 00:10:26 +0600

    on progress

--- onboarding/src/main/java/com/tarikuzzamantito/apps/onboarding/OnBoardingData.kt
+++ onboarding/src/main/java/com/tarikuzzamantito/apps/onboarding/OnBoardingData.kt
@@ -1,8 +1,12 @@
 package com.tarikuzzamantito.apps.onboarding
 
+import androidx.compose.ui.graphics.Color
+import com.tarikuzzamantito.apps.onboarding.ui.theme.ColorBlue
+
 /**
  * Created by Tarikuzzaman Tito on 8/11/2024 8:18 PM
  */
 data class OnBoardingData(
     val image: Int,
+    val backgroundColor: Color = ColorBlue
 )
--- onboarding/src/main/java/com/tarikuzzamantito/apps/onboarding/OnboardingMainActivity.kt
+++ onboarding/src/main/java/com/tarikuzzamantito/apps/onboarding/OnboardingMainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,8 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.tarikuzzamantito.apps.onboarding.ui.theme.ColorBlue
 import com.tarikuzzamantito.apps.onboarding.ui.theme.JetpackDevTheme
 
 /**
@@ -36,9 +40,9 @@ class OnboardingMainActivity : ComponentActivity() {
             JetpackDevTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     val items = ArrayList<OnBoardingData>()
-                    items.add(OnBoardingData(R.drawable.food))
-                    items.add(OnBoardingData(R.drawable.cooking))
-                    items.add(OnBoardingData(R.drawable.fruit))
+                    items.add(OnBoardingData(R.drawable.food, backgroundColor = Color(0xFF0189C5)))
+                    items.add(OnBoardingData(R.drawable.cooking, backgroundColor = Color(0xFFE4AF19)))
+                    items.add(OnBoardingData(R.drawable.fruit, backgroundColor = Color(0xFF96E172)))
 
                     val pagerState = rememberPagerState(
                         initialPage = 0,
@@ -68,10 +72,19 @@ fun OnBoardingPager(
     Box(modifier = modifier) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             HorizontalPager(state = pagerState) { page ->
-                Image(
-                    painter = painterResource(id = item[page].image),
-                    contentDescription = ""
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(item[page].backgroundColor),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Image(
+                        painter = painterResource(id = item[page].image),
+                        contentDescription = "",
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
         }
     }
