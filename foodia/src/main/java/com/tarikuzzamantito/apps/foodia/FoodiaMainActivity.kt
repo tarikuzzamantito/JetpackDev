f8acff8b7bab5d2a9f8a870a68a0c34c90ec56ad
Md.Tarikuzzaman <tarikuzzaman1@gmail.com>
2024-08-19 16:52:56 +0600

    Initial commit Foodia

--- foodia/src/main/java/com/tarikuzzamantito/apps/foodia/FoodiaMainActivity.kt
+++ foodia/src/main/java/com/tarikuzzamantito/apps/foodia/FoodiaMainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tarikuzzamantito.apps.foodia.ui.home.Feed
 import com.tarikuzzamantito.apps.foodia.ui.theme.JetpackDevTheme
 
 /**
@@ -21,12 +24,13 @@ class FoodiaMainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetpackDevTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            JetpackDevTheme {//Scaffold
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(name = "Android")
+                    Feed(onFoodClick = {})
                 }
             }
         }
